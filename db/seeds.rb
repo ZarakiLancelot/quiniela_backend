b6255bc4c849111a1fe27b07122a15960a11c9c7
ZarakiLancelot <kenpachi2783@gmail.com>
2022-08-31 03:34:53 -0600

    seeds de estadios

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,12 @@ User.first_or_create(email: 'einsen.vasquez.velasquez@gmail.com',
     password: 'password',
     password_confirmation: 'password',
     role: User.roles[:admin])
+
+Estadio.create!(nombre: "Estadio Icónico de Lusail", capacidad: 80000, lugar: "Municipio de Al Daayen")
+Estadio.create!(nombre: "Estadio Al Janoub", capacidad: 40000, lugar: "Municipio de Al Wakrah")
+Estadio.create!(nombre: "Estadio 974", capacidad: 40000, lugar: "Municipio de Ad Dawhah")
+Estadio.create!(nombre: "Estadio Al Thumama", capacidad: 40000, lugar: "Municipio de Ad Dawhah")
+Estadio.create!(nombre: "Estadio Internacional Khalifa", capacidad: 45416, lugar: "Municipio de Rayán")
+Estadio.create!(nombre: "Estadio Ciudad de la Educación", capacidad: 40000, lugar: "Municipio de Rayán")
+Estadio.create!(nombre: "Estadio Áhmad bin Ali", capacidad: 60000, lugar: "Municipio de Rayán")
+Estadio.create!(nombre: "Estadio Al Bayt", capacidad: 60000, lugar: "Municipio de Jor")
